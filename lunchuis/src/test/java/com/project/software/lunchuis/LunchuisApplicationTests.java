a7e2b2721ef4572eb507799fb37af7a5a342c3b9
Kevin <castrokevin312@gmail.com>
2024-12-01 22:30:01 -0500

    SPring

---
+++ lunchuis/src/test/java/com/project/software/lunchuis/LunchuisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.software.lunchuis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
